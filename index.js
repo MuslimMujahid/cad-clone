1c4fb0ec3fa4f1f5ddc016646134a63651661756
Dhafin Rayhan <dhafin@students.itb.ac.id>
2021-02-14 18:04:08 +0700

    add function to change size of existing square

--- index.js
+++ index.js
@@ -251,6 +251,11 @@ function replaceShape(index, newShapeArray) {
     drawAll();
 }
 
+function changeSquareSize(index, size) {
+    newSquare = shapeItems[index];
+    newSquare.size = size;
+    replaceShape(index, newSquare);
+}
 
 var triangle1 = {
     type: 'triangle',
@@ -272,3 +277,5 @@ shapeItems.push(triangle1);
 shapeItems.push(square1);
 
 drawAll();
+
+changeSquareSize(2, 0.6);
